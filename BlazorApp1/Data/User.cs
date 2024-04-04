fda072ced85627187092ab0289e5e4e69ef1905f
rhanc <hancockr@etsu.edu>
2024-04-04 13:49:47 -0400

    Add project files.

---
+++ BlazorApp1/Data/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    class User {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
